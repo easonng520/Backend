8b551e588df146c938b974ab7b4743eb8beb4f4f
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-11 05:45:59 +0000

    force: true will drop the table if it already exists

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const Role = db.role;
 
  db.sequelize.sync();
 // force: true will drop the table if it already exists
-/*
+
 db.sequelize.sync({force: true}).then(() => {
   console.log('Drop and Resync Database with { force: true }');
   initial();
 });
-*/
+
 
 // simple route
 app.get("/", (req, res) => {
