631b0da5671d977953f9cc483916bfafaeac8a29
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-12 02:17:36 +0000

    Display Done

--- app/models/index.js
+++ app/models/index.js
@@ -28,12 +28,15 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
 
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
   otherKey: "userId"
 });
+
 db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
