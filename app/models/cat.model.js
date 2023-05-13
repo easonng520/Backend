c14c9d41df01c1fb6a9bbc6281088af353f4fc88
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-13 08:00:35 +0000

    table cats done

--- app/models/cat.model.js
+++ app/models/cat.model.js
@@ -3,6 +3,36 @@ module.exports = (sequelize, Sequelize) => {
     title: {
       type: Sequelize.STRING
     },
+ breed: {
+      type: Sequelize.STRING
+    },
+      microchip: {
+      type: Sequelize.INTEGER
+    },
+       DOB: {
+      type: Sequelize.DATE
+    },
+    status: {
+      type: Sequelize.INTEGER
+    },
+          sex: {
+      type: Sequelize.INTEGER
+    },
+          name: {
+      type: Sequelize.INTEGER
+    },
+          image: {
+      type: Sequelize.INTEGER
+    },
+          remark: {
+      type: Sequelize.INTEGER
+    },
+      centre: {
+      type: Sequelize.STRING
+    },
+    title: {
+      type: Sequelize.STRING
+    },
     description: {
       type: Sequelize.STRING
     },
--- server.js
+++ server.js
@@ -26,10 +26,10 @@ const Breed = db.breed;
  db.sequelize.sync();
 // force: true will drop the table if it already exists
 
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
-  initial();
-});
+//db.sequelize.sync({force: true}).then(() => {
+ // console.log('Drop and Resync Database with { //force: true }');
+ // initial();
+//});
 
 
 // simple route
